b1fcd9e6d96b215eb8cdb4a5744b3ac5f7dc9a50
Yvonna <yvonna820@gmail.com>
2026-04-26 13:17:12 +0000

    Started framework for uploading audio files within amr, wav, and mp3 to transcribe into the app.

--- voice-call-test.py
+++ voice-call-test.py
@@ -1,5 +1,5 @@
 import whisper
 
 model = whisper.load_model("base", device = "cpu")
-result = model.transcribe("audio.amr")
+result = model.transcribe("robocall test.amr")
 print("Transcription:", result["text"])
